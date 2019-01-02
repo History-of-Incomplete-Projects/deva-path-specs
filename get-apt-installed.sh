1d769806175b8ee4356209ddc5e548ed980254fe
ahuangJM <ahuang.jm@outlook.com>
2019-01-01 18:22:54 -0700

    return true is target is installed and false if it isnt

--- get-apt-installed.sh
+++ get-apt-installed.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 apt list --installed >> ./apt-installed
+if grep -Fxq "$1" ./apt-installed
+then
+    echo "true"
+else
+    echo "false"
+fi
--- get-pip-installed.sh
+++ get-pip-installed.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 pip list >> ./pip-installed
+if grep -Fxq "$1" ./pip-installed
+then
+    echo "true"
+else
+    echo "false"
+fi
